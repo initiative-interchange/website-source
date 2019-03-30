8abe6afa3f65b6dfa9da84f98a1704f56cc1ad61
Michael Kudler <michael.kudler@aon.at>
2019-03-30 19:01:34 +0100

    changes

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,13 @@
 const path = require('path');
 
+const assetFolder = path.resolve(__dirname, 'assets')
+
 module.exports = {
   entry: {
     landing_page: './_js/landing_page.js'
   },
-  mode: 'development',
   output: {
-    path: path.resolve(__dirname, 'assets/js')
-  }
+    path: path.join(assetFolder, 'js')
+  },
+  mode: 'development'
 };
